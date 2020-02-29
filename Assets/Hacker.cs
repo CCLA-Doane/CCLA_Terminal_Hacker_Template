a70e8c8a5d8376a48cd12bd8ae24f11c86033dd9
Ryan Mueller <ryan.mueller@doane.edu>
2020-02-29 15:38:30 -0600

    Template Project with assets and script for Terminal Hacker Game

---
+++ Assets/Hacker.cs
@@ -0,0 +1,50 @@
+/*
+* Hacker.cs
+* This is a multiline comment. It is generally
+* considered good practice to type a description
+* of what the file is supposed to do.
+* This class will handle the basic functionality of a terminal
+* hacker game.
+*/
+
+// This is a single line comment
+
+// Below, using statements. This tells
+// the compiler what to pull from
+// if it needs other things
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    // Game configuration data
+    // Here, create variables for:
+
+    // menuHint
+
+    // level1Passwords
+
+    // level2Passwords
+
+    // level3Passwords
+
+    /* Game state
+    * a game state handles what the player
+    * is doing at a given moment:
+    * main menu, winning, losing, playing.
+    */
+
+    // level variable
+
+    // Screen variables
+
+    //password variables
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+
+    
+}
